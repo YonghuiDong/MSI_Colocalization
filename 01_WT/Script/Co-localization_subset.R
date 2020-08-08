93faf6dcdff6fcbe9a9935f1e225afc876d3d79d
YonghuiDong <yonghui.dong@gmail.com>
2020-08-08 20:19:27 +0300

    Update Co-localization_subset.R

--- 01_WT/Script/Co-localization_subset.R
+++ 01_WT/Script/Co-localization_subset.R
@@ -1,5 +1,6 @@
 #(1) load package and data
 library(Cardinal)
+library(BiocParallel)
 load("Result/MG_processed.rda")
 
 #(2) subset and colocalization analysis of DHB 409
@@ -27,25 +28,3 @@ coloc_611_sub10 <- colocalized(MG_sub10, mz= 611.161, n = 100, BPPARAM=SerialPar
 save(coloc_611_sub10, file = "Result/coloc_611_sub10.rda", compress = "xz")
 
 
-#(4) subset and colocalization analysis of tomatine 1034, it takes about 11.25 min
-pixel = dim(MG2@elementMetadata)[1]
-MG_sub100 <- MG2[, c(seq(1, pixel, 100))]
-start.time <- Sys.time()
-coloc_1034_sub100 <- colocalized(MG_sub100, mz= 1034.553, n = 100, BPPARAM=SerialParam())
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-time.taken
-save(coloc_1034_sub100, file = "Result/coloc_1034_sub100.rda", compress = "xz")
-
-
-## subset every 10 pixel
-MG_sub10 <- MG2[, c(seq(1, pixel, 10))]
-start.time <- Sys.time()
-coloc_1034_sub10 <- colocalized(MG_sub10, mz= 1034.553, n = 100, BPPARAM=SerialParam())
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-time.taken
-save(coloc_1034_sub10, file = "Result/coloc_1034_sub10.rda", compress = "xz")
-
-
-time.taken
